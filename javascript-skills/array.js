0b543503b31fa752dd1687ae5eea96b87106d4fa
2-chanhee <l01083582454@gmail.com>
2022-05-05 21:00:26 +0900

    array

---
+++ javascript-skills/array.js
@@ -0,0 +1,15 @@
+const arr = [1, 2, 3, 4, 5];
+
+// slice - 원본 배열 유지
+console.log(arr.slice(1, 3)); // index 1 ~ 3 자름 [2, 3]
+
+// splice 원본 배열 변경
+console.log(arr.splice(1, 3)); // index 1부터 3개 자름 [2,3,4]
+
+// push - array 맨 뒤에 추가
+
+// pop array 맨 뒤에서 제거
+
+// unshift - array 맨 앞에 추가
+
+// shift - array 맨 앞에 제거
