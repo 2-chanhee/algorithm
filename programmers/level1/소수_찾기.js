c4ab909cb964961960ba662f000067d48bb7a423
2-chanhee <l01083582454@gmail.com>
2022-05-28 22:56:01 +0900

    소수 찾기

--- programmers/level1/소수_찾기.js
+++ programmers/level1/소수_찾기.js
@@ -1,3 +1,4 @@
+// 방법 1
 function solution(n) {
   let arr = [];
 
@@ -18,4 +19,26 @@ function solution(n) {
 
   // 0인 것들은 소수가 아님, 0빼고 return
   return arr.filter((v) => v !== 0).length;
+}
+
+// 방법 2
+function get_primes(num) {
+  const prime = [0, 0, ...Array(num - 1).fill(1)]; // 0과 1은 소수가 아니기에 0으로 선언
+
+  console.log(prime);
+
+  for (let i = 2; i * i <= num; i += 1) {
+    // prime[i]라면, 즉 0이 아니라면 (0인건 이미 소수로 판별된 것)
+    if (prime[i]) {
+      for (let j = i * 2; j <= num; j += i) {
+        prime[j] = false;
+      }
+    }
+  }
+
+  return prime.filter(Boolean); // true를 고른다, 즉 1을 고름
+}
+
+function solution(n) {
+  return get_primes(n).length;
 }
