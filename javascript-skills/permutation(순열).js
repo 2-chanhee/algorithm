3853bc15eeee2cfbd4ebd62d1911580adbd125d4
2-chanhee <l01083582454@gmail.com>
2022-05-29 17:56:48 +0900

    순열

---
+++ javascript-skills/permutation(순열).js
@@ -0,0 +1,30 @@
+/**
+ * 순열
+ * 순서 상관있음
+ */
+function permutations(arr, n) {
+  // 1개만 뽑는다면 그대로 순열을 반환한다. 탈출 조건으로도 사용된다.
+  if (n === 1) return arr.map((v) => [v]);
+  let result = [];
+
+  // 요소를 순환한다
+  arr.forEach((fixed, idx, arr) => {
+    // 현재 index를 제외한 요소를 추출한다.
+    // index번째는 선택된 요소
+    const rest = arr.filter((_, index) => index !== idx);
+    console.log("rest", rest);
+    // 선택된 요소를 제외하고 재귀 호출한다.
+    const perms = permutations(rest, n - 1);
+    console.log("perms", perms);
+    // 선택된 요소와 재귀 호출을 통해 구한 순열을 합쳐준다.
+    const combine = perms.map((v) => [fixed, ...v]);
+    console.log("combine", combine);
+    // 결과 값을 추가한다.
+    result.push(...combine);
+  });
+
+  // 결과 반환
+  return result;
+}
+
+console.log(permutations([1, 2], 2));
